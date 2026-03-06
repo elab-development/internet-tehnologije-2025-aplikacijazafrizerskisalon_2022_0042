471323c51b3eb126d88578c012ac3981ffc9ada9
Valentina Stankovic <valentinastankovic824@gmail.com>
2026-03-06 18:19:10 +0100

    dodat vercel url u cors

--- salon-api/config/cors.php
+++ salon-api/config/cors.php
@@ -6,6 +6,7 @@ return [
         'http://localhost',
         'http://localhost:5173',
         'http://127.0.0.1:8000',
+        'https://internet-tehnologije-2025-aplikacij-one.vercel.app',
     ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['Content-Type', 'Authorization', 'X-Requested-With'],
